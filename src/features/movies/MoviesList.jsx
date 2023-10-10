499095dc547d277b394c2a3847970e0508cd37bb
borodinka <sofibor03@gmail.com>
2023-10-10 21:46:56 +0200

    Add test popular movies data

--- src/features/movies/MoviesList.jsx
+++ src/features/movies/MoviesList.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import { data } from "./data";
 
 function MoviesList() {
   return (
     <div>
-      MoviesList
-      <Link to={"/movies/dsds"}>Go to movie</Link>
+      {data.results.map((movie) => (
+        <Link key={movie.id} to={`/movies/${movie.id}`}>
+          {movie.title}
+        </Link>
+      ))}
     </div>
   );
 }
