86e640b75878bd373face7d1a412c1e68481acd4
borodinka <sofibor03@gmail.com>
2023-10-11 10:45:24 +0200

    Add SimpleGrid alignment

--- src/features/movies/MoviesList.jsx
+++ src/features/movies/MoviesList.jsx
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom";
 import { data } from "./data";
 import MovieCard from "./MovieCard";
+import { SimpleGrid } from "@chakra-ui/react";
 
 function MoviesList() {
   return (
-    <div>
+    <SimpleGrid spacing={4} columns={{ sm: 1, md: 3, lg: 4 }}>
       {data.results.map((movie) => (
         <Link key={movie.id} to={`/movies/${movie.id}`}>
           {movie.title}
           <MovieCard />
         </Link>
       ))}
-    </div>
+    </SimpleGrid>
   );
 }
 
