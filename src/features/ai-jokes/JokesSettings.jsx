6bd222ec82a3f1d94fee8585eddfcc749eee0f6d
borodinka <sofibor03@gmail.com>
2023-10-13 21:54:25 +0200

    Customise JokesSettings, add functionalities

--- src/features/ai-jokes/JokesSettings.jsx
+++ src/features/ai-jokes/JokesSettings.jsx
@@ -1,5 +1,13 @@
+import { DeleteIcon, SettingsIcon } from "@chakra-ui/icons";
 import {
+  Badge,
   Button,
+  Flex,
+  FormControl,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftAddon,
   Popover,
   PopoverArrow,
   PopoverBody,
@@ -9,23 +17,97 @@ import {
   PopoverHeader,
   PopoverTrigger,
   Portal,
+  Text,
 } from "@chakra-ui/react";
+import { useState } from "react";
 
 function JokesSettings() {
+  const [form, setForm] = useState({ name: "", description: "" });
+  const [rules, setRules] = useState([
+    { name: "Joke type", description: "Programmer" },
+  ]);
+
+  const handleRuleRemove = (ruleName) => {
+    const ruleIndex = rules.findIndex((rule) => rule.name === ruleName);
+    if (ruleIndex < 0) return;
+
+    const newRules = [...rules];
+    newRules.splice(ruleIndex, 1);
+
+    setRules(newRules);
+  };
+
+  const handleAddRule = (event) => {
+    event.preventDefault();
+    setRules([...rules, form]);
+  };
+
   return (
-    <Popover>
+    <Popover placement="bottom-start">
       <PopoverTrigger>
-        <Button>Settings</Button>
+        <Button leftIcon={<SettingsIcon />} variant="solid">
+          Settings
+        </Button>
       </PopoverTrigger>
       <Portal>
         <PopoverContent>
           <PopoverArrow />
-          <PopoverHeader>Header</PopoverHeader>
+          <PopoverHeader>Joke Rules</PopoverHeader>
           <PopoverCloseButton />
           <PopoverBody>
-            <Button colorScheme="blue">Button</Button>
+            <Flex width="100%" direction="column">
+              {rules.map((rule) => (
+                <Flex
+                  key={rule.name}
+                  width="100%"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Text>
+                    <Badge mr={2} colorScheme="purple">
+                      {rule.name}
+                    </Badge>
+                    <small>{rule.description}</small>
+                  </Text>
+                  <IconButton
+                    onClick={() => handleRuleRemove(rule.name)}
+                    variant="outline"
+                    icon={<DeleteIcon />}
+                    size="sm"
+                  />
+                </Flex>
+              ))}
+            </Flex>
           </PopoverBody>
-          <PopoverFooter>This is the footer</PopoverFooter>
+          <PopoverFooter>
+            <form onSubmit={handleAddRule}>
+              <FormControl isRequired>
+                <InputGroup size="sm" mb={2}>
+                  <InputLeftAddon>Name:</InputLeftAddon>
+                  <Input
+                    value={form.name}
+                    onChange={(e) => setForm({ ...form, name: e.target.value })}
+                    type="text"
+                  />
+                </InputGroup>
+              </FormControl>
+              <FormControl isRequired>
+                <InputGroup size="sm" mb={2}>
+                  <InputLeftAddon>Description:</InputLeftAddon>
+                  <Input
+                    value={form.description}
+                    onChange={(e) =>
+                      setForm({ ...form, description: e.target.value })
+                    }
+                    type="text"
+                  />
+                </InputGroup>
+              </FormControl>
+              <Button size="sm" type="submit" alignSelf="flex-end">
+                Submit
+              </Button>
+            </form>
+          </PopoverFooter>
         </PopoverContent>
       </Portal>
     </Popover>
