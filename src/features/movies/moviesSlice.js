e4ff24f56c8caeb5cda9cc1baeeafd55fc470101
borodinka <sofibor03@gmail.com>
2023-10-15 14:57:37 +0200

    Add thunk with extraReducers to fetch movies

--- src/features/movies/moviesSlice.js
+++ src/features/movies/moviesSlice.js
@@ -1,14 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { data } from "./data";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchPopularMovies } from "./api";
+
+const initialState = {
+  movies: [],
+  status: "idle", // 'idle' / 'loading' / 'succeeded' / 'failed'
+  eror: null,
+};
+
+export const fetchMovies = createAsyncThunk("movies/fetchMovies", async () => {
+  const data = await fetchPopularMovies();
+  return data.data;
+});
 
 const moviesSlive = createSlice({
   name: "movies",
-  initialState: data.results,
+  initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchMovies.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchMovies.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.movies = action.paylod.results;
+      })
+      .addCase(fetchMovies.rejected, (state, action) => {
+        state.status = "failed";
+        state.eror = action.error.message;
+      });
+  },
 });
 
 export const selectAllMovies = (state) => state.movies;
-
+export const selectMoviesStatus = (state) => state.status;
+export const selectMoviesError = (state) => state.error;
 export const selectMovieById = (state, movieId) =>
   state.movies.find((movie) => movie.id === Number(movieId));
 
