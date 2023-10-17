f2c6c62e53f7771863587327d383214b4674eb2d
borodinka <sofibor03@gmail.com>
2023-10-17 11:26:13 +0200

    Add handleGenerateJoke logic

--- src/features/ai-jokes/JokesGenerator.jsx
+++ src/features/ai-jokes/JokesGenerator.jsx
@@ -1,12 +1,33 @@
-import { Button } from "@chakra-ui/react";
-import { useSelector } from "react-redux";
-import { selectJokeByMovieId, selectJokesStatus } from "./aiJokesSlice";
+import { Button, useToast } from "@chakra-ui/react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchJoke,
+  selectJokeByMovieId,
+  selectJokesStatus,
+} from "./aiJokesSlice";
 
 function JokesGenerator({ movieId, movieTitle, movieDescription }) {
+  const toast = useToast();
+  const dispatch = useDispatch();
   const joke = useSelector((state) => selectJokeByMovieId(state, movieId));
   const jokeStatus = useSelector(selectJokesStatus);
 
-  const handleGenerateJoke = () => {};
+  const handleGenerateJoke = async () => {
+    try {
+      await dispatch(
+        fetchJoke({ movieId, movieTitle, movieDescription })
+      ).unwrap();
+    } catch (err) {
+      toast({
+        title: "Failed to load joke",
+        description:
+          "Please refresh the page and check your internet connection",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  };
 
   return (
     <Button
