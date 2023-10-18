64fa864a575f1b202987fb16b9394a3fce87f615
borodinka <sofibor03@gmail.com>
2023-10-18 10:28:22 +0200

    Display movie rating

--- src/features/movies/MovieDetails.jsx
+++ src/features/movies/MovieDetails.jsx
@@ -1,4 +1,5 @@
 import {
+  Badge,
   Box,
   Card,
   CardBody,
@@ -46,6 +47,9 @@ function MovieDetails() {
         <Stack>
           <CardBody>
             <Heading size="md">{movie.title}</Heading>
+            <Text py="2" color="gray.680">
+              Rating: <Badge colorScheme="yellow">{movie?.vote_average}</Badge>
+            </Text>
             <Text py="2">{movie.overview}</Text>
           </CardBody>
           <CardFooter>
